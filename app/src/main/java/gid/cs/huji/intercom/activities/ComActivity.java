0d0362b4e58a6257aed6da5560dd810c76b5877a
gideonbar <gidonbar@gmail.com>
2015-03-24 15:17:42 +0200

    got binding on XWalckWebView to work

--- app/src/main/java/gid/cs/huji/intercom/activities/ComActivity.java
+++ app/src/main/java/gid/cs/huji/intercom/activities/ComActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import org.xwalk.core.XWalkPreferences;
 import org.xwalk.core.XWalkView;
 
+import gid.cs.huji.intercom.JSBind.JS_Com_Bind;
 import gid.cs.huji.intercom.R;
 import gid.cs.huji.intercom.model.Personnel;
 import gid.cs.huji.intercom.services.PersonnelService;
@@ -15,10 +16,13 @@ import gid.cs.huji.intercom.services.PersonnelService;
 /**
  * Created by gideonbar on 08/03/15.
  */
-public class ComActivity extends Activity
-{
-    private XWalkView xWalkWebView;
+public class ComActivity extends Activity{
+
     private static final String TAG = ComActivity.class.getSimpleName();
+
+    public static final String COM_URL = "http://e-10.cs.huji.ac.il:8000/intercom/webrtc_example/";
+
+    private XWalkView xWalkView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,38 +35,41 @@ public class ComActivity extends Activity
         Personnel personnel = (Personnel)bundle.getSerializable(Personnel.PERSONNEL);
         Log.d(TAG, "Muhta " + personnel.getBrowseText());
 
-        xWalkWebView=(XWalkView)findViewById(R.id.xwalkWebView);
-        xWalkWebView.clearCache(true);
-        xWalkWebView.load("http://e-10.cs.huji.ac.il:8000/intercom/webrtc_example/", null);
 
-        // turn on debugging
+//        Don't know how this helps if the preferences are connected?
+        XWalkPreferences.setValue("enable-javascript", true);
         XWalkPreferences.setValue(XWalkPreferences.REMOTE_DEBUGGING, true);
+
+        xWalkView=(XWalkView)findViewById(R.id.xwalkWebView);
+        xWalkView.addJavascriptInterface(new JS_Com_Bind(this, xWalkView), "Android");
+
+        xWalkView.clearCache(true);
+        xWalkView.load(COM_URL, null);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (xWalkWebView != null) {
-            xWalkWebView.pauseTimers();
-            xWalkWebView.onHide();
+        if (xWalkView != null) {
+            xWalkView.pauseTimers();
+            xWalkView.onHide();
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (xWalkWebView != null) {
-            xWalkWebView.resumeTimers();
-            xWalkWebView.onShow();
+        if (xWalkView != null) {
+            xWalkView.resumeTimers();
+            xWalkView.onShow();
         }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (xWalkWebView != null) {
-            xWalkWebView.onDestroy();
+        if (xWalkView != null) {
+            xWalkView.onDestroy();
         }
     }
-
 }
